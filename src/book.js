9a40522ab2994ac04020c1f11244ee7855212c91
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-02-03 21:04:02 -0700

    Add warmup, book.js and book-test.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,52 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+}
+
+function saveReview(reviewPhrase, arrayOfReviews){
+  for(var i = 0; i < arrayOfReviews.length; i++){
+    var actualReview = arrayOfReviews[i]
+   if(actualReview === reviewPhrase){
+     return arrayOfReviews
+    }
+  }
+  arrayOfReviews.push(reviewPhrase)
+    return arrayOfReviews
+}
+
+
+function calculatePageCount(title) {
+  return (title.length * 20)
+}
+
+function writeBook(title, mainCharacter, genre){
+ return {
+      title: title,
+      mainCharacter: mainCharacter,
+      pageCount: calculatePageCount(title),
+      genre: genre,
+    }
+  }
+
+
+function editBook(book) {
+  return (book.pageCount *= .75)
+}
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
