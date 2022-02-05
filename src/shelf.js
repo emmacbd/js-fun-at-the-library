9968a562f3b5774ee2d77c3a6affda0d69b004f9
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-02-04 18:47:17 -0700

    Add Shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,41 @@
 
-module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+function shelfBook(title, sciFiShelf) {
+  if(title.genre === "sciFi" && sciFiShelf.length < 3){
+  sciFiShelf.unshift(title)
+  }
+}
+
+
+function unshelfBook(book, shelf) {
+  for(var i = 0; i < shelf.length; i++) {
+    if(shelf[i].title == book){
+    shelf.splice(i, 1)
+  }
+}
+  return shelf
+ }
+
+ function listTitles(books) {
+   var bookTitles = []
+   for(var i = 0; i < books.length; i++) {
+     bookTitles.push(books[i].title);
+}  return bookTitles.join(', ');
+
 };
+
+function searchShelf(shelf, bookTitle) {
+  for(var i = 0; i < shelf.length; i++) {
+if(bookTitle === shelf[i].title){
+    return true
+  }
+ }
+   return false
+}
+
+module.exports = {
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
+
+}
